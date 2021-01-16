3265accfe58bb5dc1d30f7838b65393c29d0405c
Krishnendu Sudheesh <krishnendusudheesh4@gmail.com>
2021-01-17 01:13:52 +0530

    1

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        REACT APP , {this.state.name} some chande [Frontend v3][FINAL FRONTENT UPDATE] --- ZOOOM -- is updates working??
+        REACT APP , {this.state.name} some chande [Frontend v3][FINAL FRONTENT UPDATE] --- ZOOOM -- is updates working?? try 2
       </div>
     );
   }
